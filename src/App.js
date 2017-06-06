168a58d38738ceadb80d3b056f1914a4624f0fc1
Michael Feng <feng@Michaels-Nitro-MacBook-Pro.local>
2017-06-05 19:11:58 -0700

    page 47 of react book

--- src/App.js
+++ src/App.js
@@ -1,18 +1,78 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+const list = [
+  {
+    title: 'React',
+    url: 'https://facebook.github.io/react/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://github.com/reactjs/redux',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
+
+const isSearched = (searchTerm) =>
+  (item) =>
+    !searchTerm || 
+    item.title.toLowerCase().includes(searchTerm.toLowerCase());
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      list,
+      searchTerm: '',
+    };
+    this.onSearchChange = this.onSearchChange.bind(this);
+    this.onDismiss = this.onDismiss.bind(this);
+  }
+  onSearchChange(event) {
+    this.setState({ searchTerm: event.target.value });
+  }
+  onDismiss(id) {
+    const isNotId = (item) => item.objectID !== id;
+    const updatedList = this.state.list.filter(isNotId);
+    this.setState({ list: updatedList })
+  }
+
   render() {
+    const { list, searchTerm } = this.state;
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <form>
+          <input  type='text'
+                  value={searchTerm}
+                  onChange={this.onSearchChange}
+          />
+        </form>
+        { list.filter(isSearched(searchTerm))
+            .map((item) => 
+              <div key={item.objectID}>
+                <span>
+                  <a href={item.url}>{item.title}</a>
+                </span>
+                <span>{item.author}</span>
+                <span>{item.num_comments}</span>
+                <span>{item.points}</span>
+
+                <span>
+                  <button onClick={() => this.onDismiss(item.objectID)}
+                          type="button">
+                    Dismiss
+                  </button>
+                </span>
+              </div>
+            )
+        }
       </div>
     );
   }
